64a959918c577b1c8d14359ebe5861b4b545897d
SoniaEjaz-sonia <sejaz2510@gmail.com>
2023-02-14 20:26:06 +0500

    forgot password screen

---
+++ lib/src/features/auth/login/forgot_password/forgot_password_screen.dart
@@ -0,0 +1,107 @@
+import 'package:client_zipcare/src/core/constants/app_theme.dart';
+import 'package:client_zipcare/src/features/components/custom_widgets.dart';
+import 'package:client_zipcare/src/features/home/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  ForgotPasswordScreen({Key? key}) : super(key: key);
+
+  final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomWidgets().getAppBar('Forgot Password'),
+      body: ListView(
+        padding: const EdgeInsets.all(25),
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 18.0, right: 18.0, top: 10.0, bottom: 20.0),
+            child: Text(
+              'Enter registered email address with your account and we’ll send you a link to reset your password. ',
+              textAlign: TextAlign.justify,
+              style: GoogleFonts.lexend(
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+                color: blackColor,
+              ),
+            ),
+          ),
+          FormBuilder(
+            key: _formKey,
+            onChanged: () {
+              _formKey.currentState!.save();
+            },
+            autovalidateMode: AutovalidateMode.disabled,
+            skipDisabled: true,
+            child: Column(
+              children: [
+                FormBuilderTextField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  name: 'email',
+                  decoration: InputDecoration(
+                    labelText: 'Email Address',
+                    labelStyle: GoogleFonts.lexend(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    border: const OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: greyColor),
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  ),
+                  onChanged: (val) {},
+                  validator: FormBuilderValidators.compose(
+                    [
+                      FormBuilderValidators.required(),
+                      FormBuilderValidators.email(),
+                    ],
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: ElevatedButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeScreen(),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12), // <-- Radius
+                  ),
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 5.0),
+                    child: Text(
+                      "Submit",
+                      style: GoogleFonts.lexend(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                        color: whiteColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
