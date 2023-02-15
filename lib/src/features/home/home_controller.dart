79076b750f9b0c82db804b123d9b44a6be3acbbd
SoniaEjaz-sonia <sejaz2510@gmail.com>
2023-02-15 20:44:26 +0500

    home controller

---
+++ lib/src/features/home/home_controller.dart
@@ -0,0 +1,20 @@
+import 'package:client_zipcare/src/features/home/ui/home/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final homeProvider = ChangeNotifierProvider.autoDispose((ref) => HomeController());
+
+enum NavBarItems { home, jobs, chat, account }
+
+class HomeController extends ChangeNotifier {
+  HomeController();
+
+  int currentIndex = 0;
+
+  final List<Widget> pages = [const HomeScreen(), const Text('jobs'), const Text('chats'), const Text('account')];
+
+  void onTapped(int index) {
+    currentIndex = index;
+    notifyListeners();
+  }
+}
