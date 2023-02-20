38c80fbcf664c83d1758fc0025d9eae2ddec1f3f
SoniaEjaz-sonia <sejaz2510@gmail.com>
2023-02-20 23:26:18 +0500

    Chat window updated

--- lib/src/features/home/chat/chat_window/chat_window_controller.dart
+++ lib/src/features/home/chat/chat_window/chat_window_controller.dart
@@ -1,20 +1,189 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+import 'package:mime/mime.dart';
+import 'package:open_filex/open_filex.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 
 final chatWindowProvider = ChangeNotifierProvider.autoDispose((ref) => ChatWindowController());
 
 class ChatWindowController extends ChangeNotifier {
   ChatWindowController();
 
-  List<List<String>> messagesList = [
-    ["Thank you for accepting my application. I have sent over the service agreement for approval ", "sender", "16:50"],
-    ["I received it and happy to accept.", "receiver", "16:50"],
-    ["Great. I have sent over the invoice too. Please send the approval, so that we can start.", "sender", "16:50"],
-    ["I have approved the invoice. See you today afternoon.", "receiver", "16:50"],
-  ];
+  List<types.Message> messagesList = [];
 
-  void addMessageToList(String text, String user, String time) {
-    messagesList.add([text, user, time]);
+  final user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
+
+  void getMessagesList() async {
+    final response = await rootBundle.loadString('assets/messages.json');
+    final messages = (jsonDecode(response) as List).map((e) => types.Message.fromJson(e as Map<String, dynamic>)).toList();
+
+    messagesList = messages;
+
     notifyListeners();
+  }
+
+  void _addMessage(types.Message message) {
+    messagesList.insert(0, message);
+    notifyListeners();
+  }
+
+  void handlePreviewDataFetched(
+    types.TextMessage message,
+    types.PreviewData previewData,
+  ) {
+    final index = messagesList.indexWhere((element) => element.id == message.id);
+    final updatedMessage = (messagesList[index] as types.TextMessage).copyWith(
+      previewData: previewData,
+    );
+
+    messagesList[index] = updatedMessage;
+  }
+
+  void handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: user.id,
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
+  }
+
+  void handleAttachmentPressed(BuildContext context) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) => SafeArea(
+        child: SizedBox(
+          height: 144,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _handleImageSelection();
+                },
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('Photo'),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  handleFileSelection();
+                },
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('File'),
+                ),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('Cancel'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void handleFileSelection() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.any,
+    );
+
+    if (result != null && result.files.single.path != null) {
+      final message = types.FileMessage(
+        author: user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        id: const Uuid().v4(),
+        mimeType: lookupMimeType(result.files.single.path!),
+        name: result.files.single.name,
+        size: result.files.single.size,
+        uri: result.files.single.path!,
+      );
+
+      _addMessage(message);
+    }
+  }
+
+  void _handleImageSelection() async {
+    final result = await ImagePicker().pickImage(
+      imageQuality: 70,
+      maxWidth: 1440,
+      source: ImageSource.gallery,
+    );
+
+    if (result != null) {
+      final bytes = await result.readAsBytes();
+      final image = await decodeImageFromList(bytes);
+
+      final message = types.ImageMessage(
+        author: user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        height: image.height.toDouble(),
+        id: const Uuid().v4(),
+        name: result.name,
+        size: bytes.length,
+        uri: result.path,
+        width: image.width.toDouble(),
+      );
+
+      _addMessage(message);
+    }
+  }
+
+  void handleMessageTap(BuildContext _, types.Message message) async {
+    if (message is types.FileMessage) {
+      var localPath = message.uri;
+
+      if (message.uri.startsWith('http')) {
+        try {
+          final index = messagesList.indexWhere((element) => element.id == message.id);
+          final updatedMessage = (messagesList[index] as types.FileMessage).copyWith(
+            isLoading: true,
+          );
+
+          messagesList[index] = updatedMessage;
+
+          final client = http.Client();
+          final request = await client.get(Uri.parse(message.uri));
+          final bytes = request.bodyBytes;
+          final documentsDir = (await getApplicationDocumentsDirectory()).path;
+          localPath = '$documentsDir/${message.name}';
+
+          if (!File(localPath).existsSync()) {
+            final file = File(localPath);
+            await file.writeAsBytes(bytes);
+          }
+        } finally {
+          final index = messagesList.indexWhere((element) => element.id == message.id);
+          final updatedMessage = (messagesList[index] as types.FileMessage).copyWith(
+            isLoading: null,
+          );
+
+          messagesList[index] = updatedMessage;
+        }
+      }
+
+      await OpenFilex.open(localPath);
+    }
   }
 }
--- lib/src/features/home/chat/chat_window/chat_window_screen.dart
+++ lib/src/features/home/chat/chat_window/chat_window_screen.dart
@@ -7,6 +7,8 @@ import 'package:client_zipcare/src/core/constants/constants.dart';
 import 'package:client_zipcare/src/features/components/custom_widgets.dart';
 import 'package:client_zipcare/src/features/home/chat/chat_window/chat_window_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -16,9 +18,12 @@ class ChatWindow extends ConsumerWidget {
 
   String userName;
 
+  final _formKey = GlobalKey<FormBuilderState>();
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final chatWindowC = ref.watch(chatWindowProvider);
+    if (chatWindowC.messagesList.isEmpty) chatWindowC.getMessagesList();
 
     return Scaffold(
       appBar: PreferredSize(
@@ -53,128 +58,207 @@ class ChatWindow extends ConsumerWidget {
           ],
         ),
       ),
-      body: Stack(
-        children: <Widget>[
-          ListView.builder(
-            itemCount: chatWindowC.messagesList.length,
-            shrinkWrap: true,
-            padding: const EdgeInsets.only(top: 10, bottom: 10),
-            itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
-                alignment: Alignment.bottomCenter,
-                child: Align(
-                  alignment: (chatWindowC.messagesList[index][1] == "receiver" ? Alignment.topLeft : Alignment.topRight),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.6,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topRight: const Radius.circular(15),
-                        topLeft: const Radius.circular(15),
-                        bottomLeft: Radius.circular(chatWindowC.messagesList[index][1] == "receiver" ? 0 : 15),
-                        bottomRight: Radius.circular(chatWindowC.messagesList[index][1] == "receiver" ? 15 : 0),
-                      ),
-                      color: (chatWindowC.messagesList[index][1] == "receiver" ? shadowColor : primaryColor),
-                    ),
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          chatWindowC.messagesList[index][0],
-                          style: GoogleFonts.lexend(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                            color: chatWindowC.messagesList[index][1] == "receiver" ? blackColor : whiteColor,
-                          ),
-                        ),
-                        const SizedBox(height: 5.0),
-                        Row(
-                          mainAxisAlignment: chatWindowC.messagesList[index][1] == "receiver" ? MainAxisAlignment.start : MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              chatWindowC.messagesList[index][2],
-                              style: GoogleFonts.lexend(
-                                fontSize: 10,
-                                fontWeight: FontWeight.w400,
-                                color: chatWindowC.messagesList[index][1] == "receiver" ? hintColor : shadowColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
+      // body: Stack(
+      //   children: <Widget>[
+      //     ListView.builder(
+      //       itemCount: chatWindowC.messagesList.length,
+      //       shrinkWrap: true,
+      //       padding: const EdgeInsets.only(top: 10, bottom: 10),
+      //       itemBuilder: (context, index) {
+      //         return Container(
+      //           padding: const EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+      //           alignment: Alignment.bottomCenter,
+      //           child: Align(
+      //             alignment: (chatWindowC.messagesList[index][1] == "receiver" ? Alignment.topLeft : Alignment.topRight),
+      //             child: Container(
+      //               width: MediaQuery.of(context).size.width * 0.6,
+      //               decoration: BoxDecoration(
+      //                 borderRadius: BorderRadius.only(
+      //                   topRight: const Radius.circular(15),
+      //                   topLeft: const Radius.circular(15),
+      //                   bottomLeft: Radius.circular(chatWindowC.messagesList[index][1] == "receiver" ? 0 : 15),
+      //                   bottomRight: Radius.circular(chatWindowC.messagesList[index][1] == "receiver" ? 15 : 0),
+      //                 ),
+      //                 color: (chatWindowC.messagesList[index][1] == "receiver" ? shadowColor : primaryColor),
+      //               ),
+      //               padding: const EdgeInsets.all(16),
+      //               child: Column(
+      //                 crossAxisAlignment: CrossAxisAlignment.start,
+      //                 children: [
+      //                   Text(
+      //                     chatWindowC.messagesList[index][0],
+      //                     style: GoogleFonts.lexend(
+      //                       fontSize: 14,
+      //                       fontWeight: FontWeight.w400,
+      //                       color: chatWindowC.messagesList[index][1] == "receiver" ? blackColor : whiteColor,
+      //                     ),
+      //                   ),
+      //                   const SizedBox(height: 5.0),
+      //                   Row(
+      //                     mainAxisAlignment: chatWindowC.messagesList[index][1] == "receiver" ? MainAxisAlignment.start : MainAxisAlignment.end,
+      //                     children: [
+      //                       Text(
+      //                         chatWindowC.messagesList[index][2],
+      //                         style: GoogleFonts.lexend(
+      //                           fontSize: 10,
+      //                           fontWeight: FontWeight.w400,
+      //                           color: chatWindowC.messagesList[index][1] == "receiver" ? hintColor : shadowColor,
+      //                         ),
+      //                       ),
+      //                     ],
+      //                   ),
+      //                 ],
+      //               ),
+      //             ),
+      //           ),
+      //         );
+      //       },
+      //     ),
+      //     Align(
+      //       alignment: Alignment.bottomLeft,
+      //       child: Card(
+      //         elevation: 5,
+      //         shadowColor: shadowColor,
+      //         shape: RoundedRectangleBorder(
+      //           borderRadius: BorderRadius.circular(10),
+      //         ),
+      //         child: Container(
+      //           padding: const EdgeInsets.only(left: 10),
+      //           height: 50,
+      //           width: double.infinity,
+      //           decoration: const BoxDecoration(
+      //             color: whiteColor,
+      //             borderRadius: BorderRadius.all(
+      //               Radius.circular(10),
+      //             ),
+      //           ),
+      //           child: Row(
+      //             children: <Widget>[
+      //               GestureDetector(
+      //                 onTap: () {},
+      //                 child: Image.asset(
+      //                   chatSmiley,
+      //                   color: blackColor,
+      //                   width: 25,
+      //                 ),
+      //               ),
+      //               const SizedBox(
+      //                 width: 15,
+      //               ),
+      //               Expanded(
+      //                 child: FormBuilderTextField(
+      //                   name: 'send',
+      //                   decoration: InputDecoration(
+      //                     hintText: "Send Message",
+      //                     hintStyle: const TextStyle(
+      //                       color: hintLightColor,
+      //                       fontSize: 14,
+      //                       fontWeight: FontWeight.w500,
+      //                     ),
+      //                     suffixIcon: Image.asset(
+      //                       attachment,
+      //                       width: 25,
+      //                     ),
+      //                     border: InputBorder.none,
+      //                   ),
+      //                 ),
+      //               ),
+      //               FloatingActionButton(
+      //                 onPressed: () => chatWindowC.addMessageToList("text", "sender", "16:50"),
+      //                 elevation: 0,
+      //                 shape: const RoundedRectangleBorder(
+      //                   borderRadius: BorderRadius.only(
+      //                     topRight: Radius.circular(10),
+      //                     bottomRight: Radius.circular(10),
+      //                   ),
+      //                 ),
+      //                 child: Image.asset(sendMsg),
+      //               ),
+      //             ],
+      //           ),
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      body: Chat(
+        messages: chatWindowC.messagesList,
+        customBottomWidget: Card(
+          elevation: 5,
+          shadowColor: shadowColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Card(
-              elevation: 5,
-              shadowColor: shadowColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+          child: Container(
+            padding: const EdgeInsets.only(left: 10),
+            height: 50,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
               ),
-              child: Container(
-                padding: const EdgeInsets.only(left: 10),
-                height: 50,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  color: whiteColor,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+            ),
+            child: Row(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {},
+                  child: Image.asset(
+                    chatSmiley,
+                    color: blackColor,
+                    width: 25,
                   ),
                 ),
-                child: Row(
-                  children: <Widget>[
-                    GestureDetector(
-                      onTap: () {},
-                      child: Image.asset(
-                        chatSmiley,
-                        color: blackColor,
-                        width: 25,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 15,
-                    ),
-                    Expanded(
-                      child: FormBuilderTextField(
-                        name: 'send',
-                        decoration: InputDecoration(
-                          hintText: "Send Message",
-                          hintStyle: const TextStyle(
-                            color: hintLightColor,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          suffixIcon: Image.asset(
+                const SizedBox(
+                  width: 15,
+                ),
+                FormBuilder(
+                  key: _formKey,
+                  child: Expanded(
+                    child: FormBuilderTextField(
+                      name: 'send',
+                      decoration: InputDecoration(
+                        hintText: "Send Message",
+                        hintStyle: const TextStyle(
+                          color: hintLightColor,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        suffixIcon: InkWell(
+                          onTap: () => chatWindowC.handleAttachmentPressed(context),
+                          child: Image.asset(
                             attachment,
                             width: 25,
                           ),
-                          border: InputBorder.none,
                         ),
+                        border: InputBorder.none,
                       ),
                     ),
-                    FloatingActionButton(
-                      onPressed: () => chatWindowC.addMessageToList("text", "sender", "16:50"),
-                      elevation: 0,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        ),
-                      ),
-                      child: Image.asset(sendMsg),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+                FloatingActionButton(
+                  onPressed: () {
+                    chatWindowC.handleSendPressed(types.PartialText(text: _formKey.currentState!.fields['send']?.value));
+                    _formKey.currentState!.fields['send']?.didChange(null);
+                  },
+                  elevation: 0,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                  ),
+                  child: Image.asset(sendMsg),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
+        // onAttachmentPressed: chatWindowC.handleFileSelection,
+        onMessageTap: chatWindowC.handleMessageTap,
+        onPreviewDataFetched: chatWindowC.handlePreviewDataFetched,
+        onSendPressed: chatWindowC.handleSendPressed,
+        user: chatWindowC.user,
       ),
     );
   }
